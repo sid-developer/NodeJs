fc18029db2518946ec5f54755387d80b650bde59
sid-developer <testdeveloper510@gmail.com>
2022-08-03 18:06:35 +0530

    worked on saving

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const UserInfo = require('./models/user_info');
+// console.log(UserInfo);
+// process.exit();
 const app = express();
 
-const mongoose = require('mongoose');
-const mongodb = 'mongodb+srv://mongo_db_user:<password>@cluster0.44acw.mongodb.net/?retryWrites=true&w=majority'
+// const mongoose = require('mongoose');
+const mongodb = 'mongodb+srv://mongo_db_user:CWd9yA874sHLaxBM@cluster0.44acw.mongodb.net/?retryWrites=true&w=majority'
 
-mongoose.connect(mongodb).then( ()=> {console.log('connected')}).catch(err => {console.log('error')}) //connected mongodb using mongoose
+mongoose.connect(mongodb).then( ()=> {
+
+    console.log('connected')
+
+    app.listen(3000);
+
+}).catch( (err) => {
+    console.log('Error: '+err)
+}) //connected mongodb using mongoose
+
+
+
+// mongoose.connect(mongodb).then( () =>{
+
+//     console.log('connected');
+
+// }).catch( (err) => {
+
+//     console.log('Error:'+err);
+
+// })
 
 app.set('view engine', 'ejs');
 
-app.listen(3000);
+// app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('Hello SID');
@@ -25,6 +49,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/add-item', (req,res) =>{
+
+    const User_Info = new UserInfo({
+        name : 'Roy',
+        email : 'roy@testmail.com'
+    })
+
+    User_Info.save()
+    // .then((result)=>{
+    //     res.send(result)
+    // })
 
     // res.send('Its an another Request');
     // res.sendFile('./views/add-item.html',{root:__dirname})
