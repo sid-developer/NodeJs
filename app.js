463ebded79236e0e71f356450864858dad976602
sid-developer <testdeveloper510@gmail.com>
2022-08-11 18:01:34 +0530

    worked on fetch data, post method for data saving

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const UserInfo = require('./models/user_info');
 // process.exit();
 const app = express();
 
+app.use(express.urlencoded({extended:true}))
+
 // const mongoose = require('mongoose');
-const mongodb = 'mongodb+srv://mongo_db_user:<Password>@cluster0.44acw.mongodb.net/DB1?retryWrites=true&w=majority'
+const mongodb = 'mongodb+srv://mongo_db_user:CWd9yA874sHLaxBM@cluster0.44acw.mongodb.net/DB1?retryWrites=true&w=majority'
 
 mongoose.connect(mongodb).then( ()=> {
 
@@ -48,23 +50,57 @@ app.get('/', (req, res) => {
     res.render('index', {items}); //render file using EJS
 })
 
+app.get('/get-items', (req,res) =>{
+    UserInfo.find().then(result=>{
+
+        res.render('index', {items : result})
+
+    }).catch(err =>{
+
+    })
+})
+
+app.post('/save-item', (req, res) =>{
+
+    // res.send(req.body)
+    const savedata = UserInfo(req.body)
+    
+    savedata.save().then(result =>{
+        res.redirect('/get-items')
+
+    }).catch(err =>{
+        res.send(err)
+    })
+
+})
+
+// app.get('/get-item',(req, res) =>{
+
+//     UserInfo.findById('62f48a42a3b0d4b67ab6cfc6').then(result => {
+//         res.send(result)
+//     }).catch(err =>{
+
+//     })
+
+// })
+
 app.get('/add-item', (req,res) =>{
 
-    const User_Info = new UserInfo({
-        name : 'Roy',
-        email : 'roy@testmail.com'
-    })
+    // const User_Info = new UserInfo({
+    //     name : 'Roy',
+    //     email : 'roy@testmail.com'
+    // })
 
-    User_Info.save()
-    .then((result)=>{
-        res.send(result)
-    }).catch((err) =>{
-        console.log('Error '+err);
-    })
+    // User_Info.save()
+    // .then((result)=>{
+    //     res.send(result)
+    // }).catch((err) =>{
+    //     console.log('Error '+err);
+    // })
 
     // res.send('Its an another Request');
     // res.sendFile('./views/add-item.html',{root:__dirname})
-    // res.render('add-item'); //render file using EJS
+    res.render('add-item'); //render file using EJS
 });
 
 app.use((req,res) =>{
