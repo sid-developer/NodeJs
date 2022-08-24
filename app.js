ef642ed64c6c728ae7c6b79005b3dc1b42c04de1
sid-developer <testdeveloper510@gmail.com>
2022-08-24 15:50:13 +0530

    worked on saved data, delete and view data

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.urlencoded({extended : true}));
 
 const mongoose = require('mongoose')
-const mongodb = 'mongodb+srv://mongo_db_user:<Password>@cluster0.5qiqpdd.mongodb.net/sample_db?retryWrites=true&w=majority'
+const mongodb = 'mongodb+srv://mongo_db_user:W85t2FUhQ9hcwBk2@cluster0.5qiqpdd.mongodb.net/sample_db?retryWrites=true&w=majority'
 
 const user_schema = require('./models/user-schema');
 
@@ -24,7 +24,7 @@ app.set('view engine','ejs')
 app.get('/', (req, res) => {
 
     user_schema.find().then( (result) => {
-        res.render('index',{items : result})
+        res.render('index',{data : result})
     }).catch( (err) =>{
         res.send('Error:'+err);
     })
@@ -45,6 +45,55 @@ app.post('/save-item', (req, res) =>{
     }).catch( (err) =>{
         res.send('failed '+err);
     })
+
+})
+
+app.get('/add-user', (req, res) =>{
+
+    res.render('add-user');
+
+})
+
+app.post('/save-user', (req, res) =>{
+
+    User_Schema = user_schema(req.body)
+    User_Schema.save().then( (result) =>{
+
+        res.redirect('/')
+
+    }).catch( (err) =>{
+        res.send('Error: '+err)
+
+    })
+
+})
+
+app.get('/user-details/:id', (req, res) =>{
+    const user_id = req.params.id;
+
+    user_schema.findById(user_id).then( (result) =>{
+
+        res.render('user-details',{'data' : result})
+
+    }).catch( (err) =>{
+        res.send("Error:"+err)
+    })
+
+})
+
+app.get('/user-delete/:id', (req, res) =>{
+
+ const id = req.params.id
+
+  user_schema.findByIdAndDelete(id).then( (result) =>{
+
+    res.redirect('/')
+
+  }).catch( (err) => {
+
+    res.send('Error:'+err)
+
+  })
 
 })
 
--- models/user-schema.js
+++ models/user-schema.js
@@ -1,18 +1,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    name : {
+const user_details = new Schema({
+
+    first_name : {
         type : String,
         required : true
     },
-    mob : {
+    last_name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        requied : true
+    },
+    gender : {
+        type : String,
+        required : true
+    },
+    // hobbies : {
+    //     type : String,
+    //     required : true
+    // },
+    city : {
+        type : String,
+        requied : true
+    },
+    about_your_self : {
         type : String,
         required : true
     }
-}, {
-    timestamps : true
-})
+},{timestamps : true})
 
-const user_schema = mongoose.model('user_details', userSchema, 'user_details')
-module.exports = user_schema
+const user_details_schema = mongoose.model('user_details', user_details, 'user_details')
+
+module.exports = user_details_schema;
